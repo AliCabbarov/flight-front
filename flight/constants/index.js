b7a98b0a5ec1e4ab1fcdefe0816f8014862dc5d3
Ali Cabbarov <ali.cab@div.edu.az>
2024-12-13 09:22:41 +0400

    fixed backend server url

--- flight/constants/index.js
+++ flight/constants/index.js
@@ -1,2 +1,2 @@
-const USER_MS_API_BASE_URL="http://localhost:8083/user-ms/api/v1"
+const USER_MS_API_BASE_URL="http://34.76.44.68:80/user-ms/api/v1"
 const FLIGHT_MS_API_BASE_URL="http://localhost:8081/flight-ms/api/v1"
