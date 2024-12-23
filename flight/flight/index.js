fcf6d28d8e74ef379914bce8aa086188eaaef153
Ali Cabbarov <ali.cab@div.edu.az>
2024-12-23 15:47:19 +0400

    deploy-flight-manage-1

--- flight/flight/index.js
+++ flight/flight/index.js
@@ -1,6 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
   const createFlightForm = document.getElementById('createFlightForm');
+  const fromSelect = document.getElementById('from');
+  const toSelect = document.getElementById('to');
   const planeIdSelect = document.getElementById('planeId');
+
+  // Fetch available destinations and populate the "from" and "to" selects
+  const fetchDestinations = async () => {
+    try {
+      const response = await apiFetch(FLIGHT_MS_API_BASE_URL, '/flights/destination', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const destinations = await response.json();
+      // Clear previous options
+      fromSelect.innerHTML = '';
+      toSelect.innerHTML = '';
+
+      // Populate "From" and "To" selects
+      destinations.forEach(destination => {
+        const option = document.createElement('option');
+        option.value = destination;
+        option.textContent = destination;
+        fromSelect.appendChild(option);
+
+        // To select, add destination options as well
+        const optionTo = document.createElement('option');
+        optionTo.value = destination;
+        optionTo.textContent = destination;
+        toSelect.appendChild(optionTo);
+      });
+    } catch (error) {
+      console.error('Error fetching destinations:', error);
+      alert('Failed to load destinations. Please try again later.');
+    }
+  };
 
   // Fetch planes and populate the select list
   const fetchPlanes = async () => {
@@ -44,6 +84,18 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     try {
+      if (flightData.departureTime >= flightData.arrivalTime) {
+        alert('Departure time must be before arrival time.');
+        return;
+      }
+      if (flightData.from === flightData.to) {
+        alert('From and To destinations must be different.');
+        return;
+      }
+      if (flightData.departureTime < new Date().toISOString()) {
+        alert('Departure time must be in the future.');
+        return;
+      }
       const response = await apiFetch(FLIGHT_MS_API_BASE_URL, '/flights', {
         method: 'POST',
         headers: {
@@ -65,7 +117,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Fetch planes when the page loads
-  fetchPlanes();
+  // Fetch destinations and planes when the page loads
+  fetchDestinations().then(r => fetchPlanes());
+  fetchPlanes().then(r => fetchDestinations());
 });
-
